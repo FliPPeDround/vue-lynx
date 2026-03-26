080128bfeee90ddfa47d8e7526f26209d8f3d17d
flippedround <734243792@qq.com>
2026-03-26 22:44:34 +0800

    feat: volar plugin and element types

---
+++ packages/vue-lynx/types/src/index.ts
@@ -0,0 +1,6 @@
+export * from './elements/view.js'
+export * from './elements/text.js'
+export * from './elements/image.js'
+export * from './elements/scroll-view.js'
+export * from './elements/list.js'
+
---
+++ packages/vue-lynx/types/src/volar-plugin.ts
@@ -0,0 +1,8 @@
+const nativeTags = ["block", "component", "template", "slot"];
+export default () => ({
+  version: 2,
+  resolveTemplateCompilerOptions(options: Record<string, any>): Record<string, any> {
+    options.isNativeTag = (tag: string) => nativeTags.includes(tag);
+    return options;
+  },
+});
