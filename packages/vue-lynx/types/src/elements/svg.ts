af63da0a4605a33f11c88d3650720f595dd778e4
flippedround <734243792@qq.com>
2026-03-29 13:49:07 +0800

    refactor(vue-lynx): refactor: automatically generate Vue component type declarations using type mapping

--- packages/vue-lynx/types/src/elements/svg.ts
+++
@@ -1,9 +0,0 @@
-import type { DefineComponent } from "vue";
-import type { SVGProps } from '@lynx-js/types'
-import type { VueLynxProps } from "../type-utils.js";
-
-declare module "vue" {
-  export interface GlobalComponents {
-    svg: DefineComponent<VueLynxProps<SVGProps>>
-  }
-}
--- packages/vue-lynx/types/src/type-utils.ts
+++
@@ -1,7 +0,0 @@
-type ExtractBindAsOn<T> = {
-  [P in keyof T as P extends `bind${infer Rest}` ? `on${Capitalize<Rest>}` : never]: T[P]
-}
-
-type NonBindProps<T> = Omit<T, `bind${string}` | 'className'>
-
-export type VueLynxProps<T> = ExtractBindAsOn<T> & NonBindProps<T>
